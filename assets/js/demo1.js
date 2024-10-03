14bbe0834d4a0ee25552e3f19b1932485664de2c
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2024-10-03 20:48:42 +0700

    Create demo1.js

---
+++ assets/js/demo1.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#demo1').sharrre({
+        share: {
+            googlePlus: true,
+            facebook: true,
+            twitter: true,
+            delicious: true
+        },
+        buttons: {
+            googlePlus: {size: 'tall'},
+            facebook: {layout: 'box_count'},
+            twitter: {count: 'vertical'},
+            delicious: {size: 'tall'}
+        },
+        hover: function(api, options){
+            $(api.element).find('.buttons').show();
+        },
+        hide: function(api, options){
+            $(api.element).find('.buttons').hide();
+        }
+    });
+});
